67b021fe288f0f2e6b62a90ce5a380e0c6cf7042
James Shen <jamesjieranshen@gmail.com>
2025-10-09 02:26:52 -0400

    Move keymap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,17 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.autoformat = false
+vim.opt.relativenumber = true
 
-local opt = vim.opt
-opt.relativenumber = false -- Relative line numbers
+vim.g.autoformat = false
 vim.g.lazyvim_python_lsp = "basedpyright"
+
 vim.filetype.add({
   extension = {
     json = "json5",
   },
 })
-vim.keymap.set("n", "<leader>o", "<cmd>Ex<cr>", { desc = "Open file explorer" })
 vim.opt.clipboard = "unnamedplus"
 vim.g.clipboard = {
   name = "OSC 52",
