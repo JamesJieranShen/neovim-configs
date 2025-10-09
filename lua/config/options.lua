2d27989d8d37eddd65e77593a2692bbd292e6506
James Shen <jamesjieranshen@gmail.com>
2025-10-09 02:19:10 -0400

    Update OSC52 Copy configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,15 +5,22 @@ vim.g.autoformat = false
 
 local opt = vim.opt
 opt.relativenumber = false -- Relative line numbers
--- opt.clipboard = "unnamedplus"
-vim.schedule(function() -- and schedule...
-  opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
-end)
 vim.g.lazyvim_python_lsp = "basedpyright"
-
 vim.filetype.add({
   extension = {
     json = "json5",
   },
 })
 vim.keymap.set("n", "<leader>o", "<cmd>Ex<cr>", { desc = "Open file explorer" })
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
