43e4c8d50950f57ee43aac6e14840ef8ebb38c3c
James Shen <jamesjieranshen@gmail.com>
2025-10-10 04:51:57 -0400

    Fancy borders

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,22 @@
 return {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        basedpyright = {
-          settings = {
-            basedpyright = {
-			  analysis = {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      basedpyright = {
+        settings = {
+          basedpyright = {
+            analysis = {
               typeCheckingMode = "off",
- 			  },
             },
           },
         },
       },
     },
-  }
+  },
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+}
