427c44aee036c8495833030f9a92669c973cb417
James Shen <jamesjieranshen@gmail.com>
2025-11-03 10:47:05 -0500

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,36 @@
 return {
-  { "vague-theme/vague.nvim",
-    lazy=false,
+  {
+    "vague-theme/vague.nvim",
+    lazy = false,
     config = function()
       require("vague").setup({
         transparent = true,
       })
-    end
+    end,
   },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    config = function()
+      require("gruvbox").setup({
+        transparent_mode = true,
+      })
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    config = function()
+      require("rose-pine").setup({
+        styles = { transparency = false },
+      })
+    end,
+  },
+  { "2giosangmitom/nightfall.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vague",
+      colorscheme = "rose-pine",
     },
   },
 }
