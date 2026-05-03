c8b1779f59ccdd9ae903241d6b13f472c28b9f00
James Shen <jamesjieranshen@gmail.com>
2026-05-03 13:14:52 -0400

    Refactoring.nvim 2.0 updates

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -1,10 +1,39 @@
 return {
   {
     "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "lewis6991/async.nvim",
+    },
     event = "VeryLazy",
     opts = function(_, opts)
-      opts.print_var_statements.cpp = { 'std::cout << "%s" << ": " << %s << std::endl;' }
-      opts.printf_statements.cpp = { 'std::cout << "%s" << std::endl;' }
+      opts.debug = {
+        print_var = {
+          code_generation = {
+            print_var = {
+              cpp = function(o)
+                return ('std::cout << "%s %s %s:" << " " << %s << std::endl;'):format(
+                  o.debug_path,
+                  o.identifier_str,
+                  o.count,
+                  o.identifier
+                )
+              end,
+            },
+          },
+        },
+        print_loc = {
+          code_generation = {
+            print_loc = {
+              cpp = function(o)
+                return ('std::cout << "%s %s" << std::endl;'):format(
+                  o.debug_path,
+                  o.count
+                )
+              end,
+            },
+          },
+        },
+      }
     end,
   },
 }
